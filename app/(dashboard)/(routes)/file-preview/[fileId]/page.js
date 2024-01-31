0c76727098dc4ebb9dd1fe7b0512145bdaa3579e
LNCxNishant <114398745+LNCxNishant@users.noreply.github.com>
2024-01-31 14:27:13 +0530

    2:38

--- app/(dashboard)/(routes)/file-preview/[fileId]/page.js
+++ app/(dashboard)/(routes)/file-preview/[fileId]/page.js
@@ -1,10 +1,32 @@
 "use client"
 import React, { useEffect } from 'react'
+import { app } from '../../../../../FirebaseConfig';
+import { doc, getDoc, getFirestore } from 'firebase/firestore';
 
 function FilePreview({params}) {
+  const db = getFirestore(app);
+  const [file, setFile] = useState();
     useEffect(()=>{
         console.log(params?.fileId)
+        params?.fileId&&getFileInfo();
     },[])
+    const getFileInfo=async()=>{
+      const docRef = doc(db, "uploadedFile", params?.fileId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+        setFile(docSnap.data());
+        
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+      const onPasswordSave=(password)=>{
+        
+      }
+
+       
+    }
   return (
     <div>FilePreview</div>
   )
